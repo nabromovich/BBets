da7fca85e8be6f257ee79603de418b824334ad97
Nathan <nabromovich@avc.edu>
2021-11-15 13:59:43 +0000

    BrLw2

---
+++ BrLw2.R
@@ -0,0 +1,16 @@
+BrLw2 <-function(numClass=35,odds=2,countoDo=637){
+  
+  days=1:365
+  countoDo = 
+    count=countoDo
+  repeatn=numClass
+  varstor=numeric()
+  brownlow = 0
+  for (i in 1:countoDo) {
+    sam=sample(days,size=numClass,replace = T)
+    y=duplicated(sam)
+    if (sum(y)>0) brownlow=brownlow+1
+    else brownlow = brownlow - odds
+  }
+  return(brownlow)
+}
